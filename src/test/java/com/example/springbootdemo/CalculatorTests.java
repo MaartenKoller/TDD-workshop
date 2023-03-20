c86e9fef37a1f7cfd3c3acba6bad029f3ae21a80
Maarten <maartenkoller@gmail.com>
2023-03-20 22:24:33 +0100

    Workshop part 1 written step by step

--- src/test/java/com/example/springbootdemo/CalculatorTests.java
+++ src/test/java/com/example/springbootdemo/CalculatorTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CalculatorTest {
+class CalculatorTests {
 
     Calculator calculator;
 
