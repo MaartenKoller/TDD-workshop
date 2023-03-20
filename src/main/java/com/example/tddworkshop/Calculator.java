f6d930cc0f4d278d1cab7614ff0533a0b4c87738
Maarten <maartenkoller@gmail.com>
2023-03-20 22:26:17 +0100

    Workshop part 1 written step by step

---
+++ src/main/java/com/example/tddworkshop/Calculator.java
@@ -0,0 +1,5 @@
+package com.example.tddworkshop;
+
+public class Calculator {
+
+}
--- src/test/java/com/example/springbootdemo/CalculatorTests.java
+++ src/test/java/com/example/springbootdemo/CalculatorTests.java
@@ -1,5 +1,6 @@
 package com.example.springbootdemo;
 
+import com.example.tddworkshop.Calculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 
