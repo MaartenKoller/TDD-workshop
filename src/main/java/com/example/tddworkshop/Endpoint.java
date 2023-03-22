10d8ed35d7876b9b02d72abd381e59c4a20f34b6
Maarten <maartenkoller@gmail.com>
2023-03-22 13:34:41 +0100

    Workshop part 2 - integration testing

--- src/main/java/com/example/tddworkshop/Endpoint.java
+++ src/main/java/com/example/tddworkshop/Endpoint.java
@@ -1,5 +1,6 @@
 package com.example.tddworkshop;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -7,9 +8,9 @@ public class Endpoint {
 
 //    private Calculator calculator;
 
-//    @GetMapping(path="/sum")
-//    public int sumStuff() {
-//        return 1;
-//    }
+    @GetMapping(path = "/sum")
+    public int sumStuff() {
+        return 1;
+    }
 
 }
--- src/test/java/com/example/tddworkshop/EndpointIT.java
+++ src/test/java/com/example/tddworkshop/EndpointIT.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -19,9 +20,12 @@ class EndpointIT {
     private MockMvc mockMvc;
 
     @Test
-    private void sumEndpoint() throws Exception {
+    public void sumEndpoint() throws Exception {
         // call the endpoint and get a result
         ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/sum"));
         response.andExpect(MockMvcResultMatchers.status().isOk());
+        response.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+        System.out.println(response.andReturn().getResponse().getContentType());
+        System.out.println(response.andReturn().getResponse().getContentAsString());
     }
 }
